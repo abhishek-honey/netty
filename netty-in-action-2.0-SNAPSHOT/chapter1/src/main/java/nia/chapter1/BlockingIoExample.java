ec14029c2d0eb1c845c2370c274454790d5f2e45
abhishek-honey <abhishek.honey.git@gmail.com>
2020-09-28 00:00:22 +0530

    # Refracted chapter 1 code.

--- netty-in-action-2.0-SNAPSHOT/chapter1/src/main/java/nia/chapter1/BlockingIoExample.java
+++ netty-in-action-2.0-SNAPSHOT/chapter1/src/main/java/nia/chapter1/BlockingIoExample.java
@@ -8,7 +8,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * Created by kerr.
+ * Created by honey.
  *
  * Listing 1.1 Blocking I/O example
  */
@@ -17,22 +17,34 @@ public class BlockingIoExample {
 	/**
 	 * Listing 1.1 Blocking I/O example
 	 */
-	public void serve(int portNumber) throws IOException {
-		ServerSocket serverSocket = new ServerSocket(portNumber);
-		Socket clientSocket = serverSocket.accept();
-		BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-		PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-		String request, response;
-		while ((request = in.readLine()) != null) {
-			if ("Done".equals(request)) {
-				break;
+	public void serve(int portNumber) {
+		try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+			Socket clientSocket = serverSocket.accept();
+			BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+
+			String request;
+			String response;
+
+			while ((request = in.readLine()) != null) {
+				if ("Done".equals(request)) {
+					break;
+				}
+				response = processRequest(request);
+				out.println(response);
 			}
-			response = processRequest(request);
-			out.println(response);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
 	private String processRequest(String request) {
+		System.out.println(request);
 		return "Processed";
+	}
+
+	public static void main(String[] args) {
+		BlockingIoExample blockingIoExample = new BlockingIoExample();
+		blockingIoExample.serve(25);
 	}
 }
--- netty-in-action-2.0-SNAPSHOT/chapter1/src/main/java/nia/chapter1/ConnectExample.java
+++ netty-in-action-2.0-SNAPSHOT/chapter1/src/main/java/nia/chapter1/ConnectExample.java
@@ -11,40 +11,41 @@ import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
 
 /**
- * Created by kerr.
+ * Created by honey.
  *
  * Listing 1.3 Asynchronous connect
  *
  * Listing 1.4 Callback in action
  */
 public class ConnectExample {
-    private static final Channel CHANNEL_FROM_SOMEWHERE = new NioSocketChannel();
+	private static final Channel CHANNEL_FROM_SOMEWHERE = new NioSocketChannel();
 
-    /**
-     * Listing 1.3 Asynchronous connect
-     *
-     * Listing 1.4 Callback in action
-     * */
-    public static void connect() {
-        Channel channel = CHANNEL_FROM_SOMEWHERE; //reference form somewhere
-        // Does not block
-        ChannelFuture future = channel.connect(
-                new InetSocketAddress("192.168.0.1", 25));
-        future.addListener(new ChannelFutureListener() {
-            @Override
-            public void operationComplete(ChannelFuture future) {
-                if (future.isSuccess()) {
-                    ByteBuf buffer = Unpooled.copiedBuffer(
-                            "Hello", Charset.defaultCharset());
-                    ChannelFuture wf = future.channel()
-                            .writeAndFlush(buffer);
-                    // ...
-                } else {
-                    Throwable cause = future.cause();
-                    cause.printStackTrace();
-                }
-            }
-        });
+	/**
+	 * Listing 1.3 Asynchronous connect
+	 *
+	 * Listing 1.4 Callback in action
+	 */
+	public static void connect() {
+		Channel channel = CHANNEL_FROM_SOMEWHERE; // reference form somewhere
+		// Does not block
+		ChannelFuture future = channel.connect(new InetSocketAddress("192.168.0.1", 25));
+		future.addListener(new ChannelFutureListener() {
+			@Override
+			public void operationComplete(ChannelFuture future) {
+				if (future.isSuccess()) {
+					ByteBuf buffer = Unpooled.copiedBuffer("Hello", Charset.defaultCharset());
+					ChannelFuture wf = future.channel().writeAndFlush(buffer);
+					// ...
+				} else {
+					Throwable cause = future.cause();
+					cause.printStackTrace();
+				}
+			}
+		});
 
-    }
+	}
+
+	public static void main(String[] args) {
+		connect();
+	}
 }
